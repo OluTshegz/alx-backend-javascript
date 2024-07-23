83fa1fb0908f9ce1e875a8ce28821986f609ef10
Olusegun <ayosegun2001@gmail.com>
2024-07-23 01:15:56 +0100

    0x00. ES6 Basics: JavaScript, ES6

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+    const list = [];
+    for (const item of array) {
+        list.push(appendString + item);
+    }
+  
+    return list;
+}
