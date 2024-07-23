fc3a214028c0a38e66347d384e6b4b0fb3ccc807
Olusegun <ayosegun2001@gmail.com>
2024-07-23 03:16:34 +0100

    0x00. ES6 Basics: JavaScript, ES6

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,1 +1,17 @@
 /* eslint-disable */
+export default function createIteratorObject(report) {
+    const list = [];
+    for (const item of Object.values(report.allEmployees)) {
+      list.push(...item);
+    }
+  
+    return list;
+}
+
+// export default function createIteratorObject(report) {
+//     let employees = [];
+//     for (const department of Object.values(report.allEmployees)) {
+//       employees = employees.concat(department);
+//     }
+//     return employees[Symbol.iterator]();
+// }
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,1 +1,24 @@
 /* eslint-disable */
+export default function iterateThroughObject(reportWithIterator) {
+    return Array.from(reportWithIterator).join(' | ');
+}
+
+// export default function iterateThroughObject(reportWithIterator) {
+//     // Initialize the iterator
+//     let iteratorResult = reportWithIterator.next();
+    
+//     // Variable to accumulate the result string
+//     let resultString = '';
+  
+//     // Loop through the iterator until it's done
+//     while (!iteratorResult.done) {
+//       // Append the current value followed by a separator
+//       resultString += `${iteratorResult.value} | `;
+      
+//       // Get the next value from the iterator
+//       iteratorResult = reportWithIterator.next();
+//     }
+  
+//     // Remove the trailing ' | ' and return the final string
+//     return resultString.slice(0, -3);
+// }
