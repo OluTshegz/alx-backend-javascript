8a6ce9ac1ac8bee48145c55e299e0a144b42c10c
Olusegun <ayosegun2001@gmail.com>
2024-07-24 15:31:26 +0100

    0x01. ES6 Promises: JavaScript, ES6

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,1 +1,14 @@
 /* eslint-disable */
+import { uploadPhoto, createUser } from './utils.js';
+
+export default async function asyncUploadUser() {
+    try {
+        const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+        return { photo, user, };
+    } catch (error) {
+        return {
+          photo: null,
+          user: null,
+        };
+    }
+}
--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,1 +1,12 @@
 /* eslint-disable */
+export default function loadBalancer(chinaDownload, USDownload) {
+    return Promise
+        .race([chinaDownload,
+            USDownload])
+            .then((value) => {
+                return value; // Explicitly return the value
+            })
+            .catch((error) => {
+                throw error; // Handle potential rejection
+            });
+}
