fe2c0bc1ddd4871bda4f5d73a3d7c11b5203c572
Olusegun <ayosegun2001@gmail.com>
2024-07-27 15:38:46 +0100

    Adding more eslint fixes

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,23 @@
-/* eslint-disable guard-for-in */
-/* eslint-disable no-param-reassign */
+/**
+ * `for...in` loops: iterate over `object` `properties`
+ * and can include inherited properties (for objects)
+ * `for...in` loops on `array` iterates the array's `index`
+ * `for...of` loops: iterates over the `values` of an iterable `object`
+ * (like arrays, strings, maps, sets) without
+ * enumerating inherited properties (for arrays)
+ * traditional `for` loop: access elements by their index
+ * `no-param-reassign`: avoid modifying function
+ * arguments to maintain function purity
+ * creating a new array (list) and populating it
+ * with modified elements from the input array
+ * modifying the content of the list array,
+ * it's not directly modifying the original array parameter
+ */
 export default function appendToEachArrayValue(array, appendString) {
   const list = [];
+  /* eslint-disable-next-line guard-for-in */
   for (const item of array) {
+    /* eslint-disable-next-line no-param-reassign */
     list.push(appendString + item);
   }
 
