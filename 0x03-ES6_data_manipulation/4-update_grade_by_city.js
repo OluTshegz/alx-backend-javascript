91b4b478a20ac9c9c1c522661b9ac26c66686049
Olusegun <ayosegun2001@gmail.com>
2024-07-29 23:57:07 +0100

    0x03. ES6 data manipulation: JavaScript, ES6

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,12 @@
 /** */
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
+    // Filter students by the specified city
     .filter((student) => student.location === city)
     .map((student) => {
+      // Use .find() to locate the grade for the student, if available
       const gradeInfo = newGrades.find((grade) => grade.studentId === student.id);
+      // Return student object with updated grade
       return {
         ...student,
         grade: gradeInfo ? gradeInfo.grade : 'N/A',
