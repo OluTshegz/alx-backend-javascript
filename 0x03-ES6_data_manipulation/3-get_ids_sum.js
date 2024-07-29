5bbd0a87a0ac98d8003b7c29755a7532defb625e
Olusegun <ayosegun2001@gmail.com>
2024-07-29 23:46:45 +0100

    0x03. ES6 data manipulation: JavaScript, ES6

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,4 @@
 /** */
-import getListStudents from './0-get_list_students';
-
-export default function getStudentIdsSum(students = getListStudents()) {
+export default function getStudentIdsSum(students) {
   return students.reduce((sum, student) => sum + student.id, 0);
 }
