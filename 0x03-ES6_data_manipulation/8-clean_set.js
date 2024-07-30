45bcbf01712e4773b096a12aaea9c02b4dee5738
Olusegun <ayosegun2001@gmail.com>
2024-07-30 23:12:38 +0100

    0x03. ES6 data manipulation: JavaScript, ES6

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,6 @@
 /** */
 export default function cleanSet(set, startString) {
   // If startString is an empty string, return an empty string immediately
-  // if (startString === undefined || startString.length === 0 || startString === '') {
   if (!startString || startString === undefined || startString === '' || startString.length === 0) {
     return '';
   }
@@ -10,8 +9,10 @@ export default function cleanSet(set, startString) {
   // return Array.from(set)
 
   return [...set]
-    .filter((str) => str.startsWith(startString)) // Filter values that start with `startString`
-    .map((str) => str.slice(startString.length)) // Remove the prefix `startString`
-    // from each element to Extract the rest of the string after `startString`
+    // Filter values that start with `startString`
+    .filter((str) => ((str || (str !== undefined)) ? str.startsWith(startString) : ''))
+    // Remove the prefix `startString` from each element
+    // to Extract the rest of the string after `startString`
+    .map((str) => ((str || (str !== undefined)) ? str.slice(startString.length) : ''))
     .join('-'); // Join the resulting array elements with a hyphen
 }
