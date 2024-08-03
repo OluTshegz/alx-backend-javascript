992a5bf3c0513e0ca8875a05a77e25997bb1f63c
Olusegun <ayosegun2001@gmail.com>
2024-08-03 15:15:59 +0100

    0x04. Typescript

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -12,19 +12,27 @@ const row: RowElement = {
   firstName: 'Guillaume',
   lastName: 'Salva',
 };
+// const obj = {firstName: "Guillaume", lastName: "Salva"};
 
 /**
  * Insert the row and get the new row ID.
  */
 const newRowID: RowID = CRUD.insertRow(row);
+// CRUD.insertRow(obj)
+// Insert row {firstName: "Guillaume", lastName: "Salva"}
 
 /**
  * Update the row with an additional age field.
  */
 const updatedRow: RowElement = { ...row, age: 23 };
 CRUD.updateRow(newRowID, updatedRow);
+// const updatedRow: RowElement = { firstName: "Guillaume", lastName: "Salva", age: 23 };
+// CRUD.updateRow(newRowID, updatedRow);
+// Update row 125 {firstName: "Guillaume", lastName: "Salva", age: 23}
 
 /**
  * Delete the row.
  */
 CRUD.deleteRow(newRowID);
+// CRUD.deleteRow(125);
+// // Delete row id 125
