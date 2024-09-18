bad9dceb8488b15d383a29a1da967ae8454bfb0d
Olusegun <ayosegun2001@gmail.com>
2024-09-18 23:05:16 +0100

    0x05. NodeJS Basics: Back-end, JavaScript, ES6, NodeJS, ExpressJS

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,16 @@
-// Display the welcome message when the program starts
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Listen for input from stdin (user input from the console)
-process.stdin.on('data', (userInput) => {
-  // Trim any extra whitespace and newlines from the input
-  const name = userInput.toString().trim();
-
-  // Display the user's name
-  console.log(`Your name is: ${name}`);
-
-  // Exit the program after showing the name
-  process.exit();
-});
-
-// Handle the 'exit' event of the process,
-// showing a message before the program closes
-process.on('exit', () => {
-  console.log('This important software is now closing');
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
