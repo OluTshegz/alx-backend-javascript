6a5ef36bf1f279b80641a2b7cd7ced5f8eedb10f
Olusegun <ayosegun2001@gmail.com>
2024-09-20 18:09:35 +0100

    0x06. Unittests in JS: UnitTests, Back-end, JavaScript, ES6, NodeJS, ExpressJS, Mocha

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -4,6 +4,27 @@ const chai = require('chai');
 const request = require('request');
 const expect = chai.expect; // Extract 'expect' for assertions
 
+// Test suite for the index page
+describe('Index page', () => {
+  // Test case: Checking the status code of the root route
+  it('Correct status code?', (done) => {
+    request.get('http://localhost:7865/', (error, response, body) => {
+      // Expect the status code to be 200 (OK)
+      expect(response.statusCode).to.equal(200);
+      done(); // Call done when the test finishes
+    });
+  });
+
+  // Test case: Checking the correct result (response body)
+  it('Correct result?', (done) => {
+    request.get('http://localhost:7865/', (error, response, body) => {
+      // Expect the response to be the welcome message
+      expect(body).to.equal('Welcome to the payment system');
+      done(); // Call done when the test finishes
+    });
+  });
+});
+
 // Test suite for the cart page
 describe('Cart page', () => {
   // Test case: Correct status code when `id` is a number
